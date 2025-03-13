cef93f32bf784b1be7f5bcc08bb082f26fd0ba62
ArmandoMendez86 <desarrolloweb150305@gmail.com>
2025-03-13 10:30:11 -0600

    Desarrollando carrito de compras

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import Header from "./componentes/Header";
+import Guitar from "./componentes/Guitar";
+import { db } from "./data/db";
+
+function App() {
+  const [data, setData] = useState(db);
+  const [cart, setCart] = useState([]);
+
+  function addToCart(guitar) {
+    setCart((prevCart) => [...prevCart, guitar]);
+  }
+
+  return (
+    <>
+      <Header />
+
+      <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
+
+        <div className="row mt-5">
+          {data.map((item) => (
+            <Guitar addToCart={addToCart} guitar={item} key={item.id} />
+          ))}
+        </div>
+      </main>
+
+      <footer className="bg-dark mt-5 py-5">
+        <div className="container-xl">
+          <p className="text-white text-center fs-4 mt-4 m-md-0">
+            GuitarLA - Todos los derechos Reservados
+          </p>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default App;
